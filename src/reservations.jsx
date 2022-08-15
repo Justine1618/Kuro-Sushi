1c729d8cc9f3606d7f4ea511c267e7920e9637df
Justine Craig <justincrg@gmail.com>
2022-08-15 10:24:13 -0700

    Added basic styling to forms

--- src/reservations.jsx
+++ src/reservations.jsx
@@ -5,13 +5,34 @@ import React from "react";
 const Reservations = () => {
     return (
         <div className="reservations-container">
-            <form id="reservations-form">
-                <label htmlFor="res-name">Name: </label>
-                <input type="text" id="res-name" name="res-name"></input>
-                <label htmlFor="res-size">Party Size (between 1 and 10): </label>
-                <input type="number" id="res-size" name="res-size" min="1" max="10"></input>
-                <label htmlFor="res-time">Time and Date: </label>
-                <input type="datetime-local" id="res-time" name="res-time"></input>
+            <form id="reservations-form" action="./handle_reservations.js">
+                <div className="col-25">
+                    <label htmlFor="res-fname">First Name: </label>
+                </div>
+                <div className="col-75">
+                    <input type="text" id="res-fname" name="res-fname"></input>
+                </div>
+                <div className="col-25">
+                    <label htmlFor="res-lname">Last Name: </label>
+                </div>
+                <div className="col-75">
+                    <input type="text" id="res-lname" name="res-lname"></input>
+                </div>
+                <div className="col-25">
+                    <label htmlFor="res-size">Party Size (1-10): </label>
+                </div>
+                <div className="col-75">
+                    <input type="number" id="res-size" name="res-size" min="1" max="10"></input>
+                </div>
+                <div className="col-25">
+                    <label htmlFor="res-time">Time and Date: </label>
+                </div>
+                <div className="col-75">
+                    <input type="datetime-local" id="res-time" name="res-time"></input>
+                </div>
+                <div className="submit-button">
+                    <input type="submit" value="Submit"></input>
+                </div>
             </form>
         </div>
     )
