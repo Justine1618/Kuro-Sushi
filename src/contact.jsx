53968dafd545cd10709a9e1601de7a6040572a8c
Justine Craig <justincrg@gmail.com>
2022-08-26 11:06:52 -0700

    Added behavior to form submisstion.

--- src/contact.jsx
+++ src/contact.jsx
@@ -2,40 +2,52 @@
 
 import React from "react";
 
-const Contact = () => {
-    return (
-        <div className="contact-container">
-            <form>
-                <div className="col-25">
-                    <label htmlFor="contact-fname">First Name: </label>
-                </div>
-                <div className="col-75">
-                    <input type="text" id="contact-fname" name="contact-fname"></input>
-                </div>
-                <div className="col-25">
-                    <label htmlFor="contact-lname">Last Name:</label>
-                </div>
-                <div className="col-75">
-                    <input type="text" id="contact-lname" name="contact-lname"></input>
-                </div>
-                <div className="col-25">
-                    <label htmlFor="contact-email">Email Address: </label>
-                </div>
-                <div className="col-75">
-                    <input type="email" id="contact-email" name="contact-email"></input>
-                </div>
-                <div className="col-25">
-                    <label htmlFor="contact-message">Your Message: </label>
-                </div>
-                <div className="col-75">
-                    <textarea id="contact-message" name="contact-message" rows="10" cols="30"></textarea>
-                </div>
-                <div className="submit-button">
-                    <input type="submit" value="Submit"></input>
-                </div>
-            </form>
-        </div>
-    )
-};
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        alert('Thank you for your comment, we will respond within 2 business days.');
+    }
+
+    render() {
+        return (
+            <div className="contact-container">
+                <form id="contact-form" onSubmit={this.handleSubmit}> 
+                    <div className="col-25">
+                        <label htmlFor="contact-fname">First Name: </label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="contact-fname" name="contact-fname"></input>
+                    </div>
+                    <div className="col-25">
+                        <label htmlFor="contact-lname">Last Name:</label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="contact-lname" name="contact-lname"></input>
+                    </div>
+                    <div className="col-25">
+                        <label htmlFor="contact-email">Email Address: </label>
+                    </div>
+                    <div className="col-75">
+                        <input type="email" id="contact-email" name="contact-email"></input>
+                    </div>
+                    <div className="col-25">
+                        <label htmlFor="contact-message">Your Message: </label>
+                    </div>
+                    <div className="col-75">
+                        <textarea id="contact-message" name="contact-message" rows="10" cols="30"></textarea>
+                    </div>
+                    <div className="submit-button">
+                        <input type="submit" value="Submit"></input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default Contact;
--- src/reservations.jsx
+++ src/reservations.jsx
@@ -2,40 +2,52 @@
 
 import React from "react";
 
-const Reservations = () => {
-    return (
-        <div className="reservations-container">
-            <form id="reservations-form" action="./handle_reservations.js">
-                <div className="col-25">
-                    <label htmlFor="res-fname">First Name: </label>
-                </div>
-                <div className="col-75">
-                    <input type="text" id="res-fname" name="res-fname"></input>
-                </div>
-                <div className="col-25">
-                    <label htmlFor="res-lname">Last Name: </label>
-                </div>
-                <div className="col-75">
-                    <input type="text" id="res-lname" name="res-lname"></input>
-                </div>
-                <div className="col-25">
-                    <label htmlFor="res-size">Party Size (1-10): </label>
-                </div>
-                <div className="col-75">
-                    <input type="number" id="res-size" name="res-size" min="1" max="10"></input>
-                </div>
-                <div className="col-25">
-                    <label htmlFor="res-time">Time and Date: </label>
-                </div>
-                <div className="col-75">
-                    <input type="datetime-local" id="res-time" name="res-time"></input>
-                </div>
-                <div className="submit-button">
-                    <input type="submit" value="Submit"></input>
-                </div>
-            </form>
-        </div>
-    )
-};
+class Reservations extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        alert('Thank you for your reservation! You will recieve a confirmation email shortly!');
+    }
+
+    render() {
+        return (
+            <div className="reservations-container">
+                <form id="reservations-form" onSubmit={this.handleSubmit}>
+                    <div className="col-25">
+                        <label htmlFor="res-fname">First Name: </label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="res-fname" name="res-fname"></input>
+                    </div>
+                    <div className="col-25">
+                        <label htmlFor="res-lname">Last Name: </label>
+                    </div>
+                    <div className="col-75">
+                        <input type="text" id="res-lname" name="res-lname"></input>
+                    </div>
+                    <div className="col-25">
+                        <label htmlFor="res-size">Party Size (1-10): </label>
+                    </div>
+                    <div className="col-75">
+                        <input type="number" id="res-size" name="res-size" min="1" max="10"></input>
+                    </div>
+                    <div className="col-25">
+                        <label htmlFor="res-time">Time and Date: </label>
+                    </div>
+                    <div className="col-75">
+                        <input type="datetime-local" id="res-time" name="res-time"></input>
+                    </div>
+                    <div className="submit-button">
+                        <input type="submit" value="Submit"></input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default Reservations;
