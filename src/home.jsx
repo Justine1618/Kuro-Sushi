a86560239ba65993452b47617b02f4f663b49af2
Justine Craig <justincrg@gmail.com>
2022-08-26 11:12:33 -0700

    Added hours to home page

--- src/home.jsx
+++ src/home.jsx
@@ -9,6 +9,10 @@ const Home = () => {
             <h1 className="tagline">LA's Best Sushi!</h1>
             <Slideshow />
             <br />
+            <div className="hours">
+                <p>Hours: Everyday 11am - 8pm</p>
+            </div>
+            <br />
             <div className="testimonial">
                 <div><em>"This is the best sushi that I have ever had!" </em>-Robert Dalton</div>
                 <div><em>"I really love the garlic ponzu salmon!"</em> -Justine Walters</div>
